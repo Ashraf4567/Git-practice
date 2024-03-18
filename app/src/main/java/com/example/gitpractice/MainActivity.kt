dbccfc02251ceaf8196875e43329f257d721f08e
Ashraf Mohamed <ashraf.moham456@gmail.com>
2024-03-18 20:54:02 +0200

    -delete file

--- app/src/main/java/com/example/gitpractice/MainActivity.kt
+++ app/src/main/java/com/example/gitpractice/MainActivity.kt
@@ -32,7 +32,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "good afternoon $name!",
+        text = "Hello $name!",
         modifier = modifier
     )
 }
--- app/src/main/java/com/example/gitpractice/Product.kt
+++
@@ -1,6 +0,0 @@
-package com.example.gitpractice
-
-data class Product(
-    val price: Int,
-    val name: String
-)
