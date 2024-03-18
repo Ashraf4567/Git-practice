e928ac59d644ceda69a18142c569091c5cc30144
Ashraf Mohamed <ashraf.moham456@gmail.com>
2024-03-18 16:01:37 +0200

    change profile to home

--- app/src/main/java/com/example/gitpractice/ui/ProfileScreen.kt
+++ app/src/main/java/com/example/gitpractice/ui/ProfileScreen.kt
@@ -5,8 +5,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 
 @Composable
-fun ProfileScreen() {
+fun HomeScreen() {
     Row {
-        Text(text = "this profile will stash")
+        Text(text = "this is home")
     }
 }
